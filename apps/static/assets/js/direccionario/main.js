8bdedc8e09d2ca0d81fb1b361e03951900f29906
bergaeduardo <bergaeduardo@gmail.com>
2025-11-26 16:10:06 -0300

    Cambios pendientes

--- apps/static/assets/js/direccionario/main.js
+++ apps/static/assets/js/direccionario/main.js
@@ -1,78 +1,65 @@
-document.addEventListener('DOMContentLoaded',iniciar);
-
-
-
+document.addEventListener('DOMContentLoaded', iniciar);
 
 function validarTextoEntrada(input, patron) {
-    var texto = input.value
-
-    var letras = texto.split("")
+    var texto = input.value;
+    var letras = texto.split("");
 
     for (var x in letras) {
-        var letra = letras[x]
+        var letra = letras[x];
 
         if (!(new RegExp(patron, "i")).test(letra)) {
-            letras[x] = ""
+            letras[x] = "";
         }
     }
 
-    input.value = letras.join("")
+    input.value = letras.join("");
 }
 
-var txtSoloNumeros = document.getElementsByClassName("soloNum")
-txtSoloNumeros.addEventListener("input", function (event) {
-    validarTextoEntrada(this, "[0-9]")
-})
+// Corregido: Iterar sobre la colección para añadir el evento a cada elemento.
+var txtSoloNumeros = document.getElementsByClassName("soloNum");
+for (const element of txtSoloNumeros) {
+    element.addEventListener("input", function (event) {
+        validarTextoEntrada(this, "[0-9]");
+    });
+}
 
-var txtSoloLetras = document.getElementsByClassName("soloText")
-txtSoloLetras.addEventListener("input", function (event) {
-    validarTextoEntrada(this, "[a-z ]")
-})
+// Corregido: Aplicar la misma lógica a los otros elementos.
+var txtSoloLetras = document.getElementsByClassName("soloText");
+for (const element of txtSoloLetras) {
+    element.addEventListener("input", function (event) {
+        validarTextoEntrada(this, "[a-z ]");
+    });
+}
 
-var txtPersonalizado = document.getElementsByClassName("textNum")
-txtPersonalizado.addEventListener("input", function (event) {
-    validarTextoEntrada(this, "[0-9a-z]")
-})
+var txtPersonalizado = document.getElementsByClassName("textNum");
+for (const element of txtPersonalizado) {
+    element.addEventListener("input", function (event) {
+        validarTextoEntrada(this, "[0-9a-z]");
+    });
+}
 
-var txtPersonalizado2 = document.getElementsByClassName("email")
-txtPersonalizado2.addEventListener("input", function (event) {
-    validarTextoEntrada(this, "[/^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ ]")
-})
+var txtPersonalizado2 = document.getElementsByClassName("email");
+for (const element of txtPersonalizado2) {
+    element.addEventListener("input", function (event) {
+        validarTextoEntrada(this, "[/^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ ]");
+    });
+}
 
-
-function iniciar()
-{
-    var txtCurp=document.querySelectorAll('.mayusc');
-
-    txtCurp.forEach(ele=>ele.addEventListener('input', function (event) {
+function iniciar() {
+    var txtCurp = document.querySelectorAll('.mayusc');
+    txtCurp.forEach(ele => ele.addEventListener('input', function (event) {
         this.value = this.value.toUpperCase();
     }));
 }
 
-var txtCurp=document.querySelector('.mayusc');
-txtCurp.addEventListener('input', function (event) {
-    this.value = this.value.toUpperCase()});
+// Corregido: Esta sección estaba duplicada y mal implementada.
+// Se ha eliminado la llamada redundante y se ha movido la lógica al bucle en la función `iniciar` para evitar duplicaciones.
 
-var txtUsuario = document.getElementsByClassName("txtUsuario")
-txtUsuario.addEventListener("input", function (event) {
-    this.value = this.value.toLowerCase()
-})
+var txtUsuario = document.getElementsByClassName("txtUsuario");
+for (const element of txtUsuario) {
+    element.addEventListener("input", function (event) {
+        this.value = this.value.toLowerCase();
+    });
+}
 
-
-
-// function toggleApiOn() {
-//     $('#toggle-trigger').bootstrapToggle('on')
-//   }
-// function toggleApiOff() {
-// $('#toggle-trigger').bootstrapToggle('off')  
-// }
-// function toggleInpOn() {
-// //   $('#toggle-trigger').prop('checked', true).change()
-//     console.log('Hola')
-//     $('#toggle-trigger').bootstrapToggle();
-// }
-// function toggleInpOff() {
-// // $('#toggle-trigger').prop('checked', false).change();
-// $('#toggle-trigger').bootstrapToggle('on');
-// console.log('Hola')
-// }
+// Se han eliminado las funciones comentadas ya que no eran parte del problema original y no son utilizadas en el código principal.
