4945342801073ab4534f4511ec5b1a7e520fe592
bergaeduardo <bergaeduardo@gmail.com>
2026-03-31 16:57:18 -0300

    Servir archivos media en desarrollo

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,8 @@ Copyright (c) 2019 - present AppSeed.us
 from django.contrib import admin
 from django.urls import path, include  # add this
 from rest_framework.documentation import include_docs_urls
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),          # Django admin route
@@ -20,3 +22,7 @@ urlpatterns = [
     path("Api/", include("apps.home.direcciones.urls_Api")),
     path('docs/', include_docs_urls(title='Lakers_Lab Api Documentation')),
 ]
+
+# Servir archivos media en desarrollo
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
